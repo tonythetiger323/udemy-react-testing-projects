ac22e567e6633147a36e47393f077a48c8738b5a
Tony Greeley <tonygreeley@Tonys-iMac.local>
2020-12-17 12:31:23 -0600

    Completed click-counter in my-solutions.

--- my-solutions/click-counter/src/App.js
+++ my-solutions/click-counter/src/App.js
@@ -1,10 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // set component state count and error
+  const [count, setCount] = useState(0);
+  const [error, setError] = useState(false);
+
   return (
     <div className="App" data-test="component-app">
-      <h1>App</h1>
+      <h1 data-test="counter-display">
+        Count&nbsp;
+        <span data-test="count">{count}</span>
+        </h1>
+      <button 
+      data-test="more-button"
+      onClick={() => {
+        if(error) {
+        setError(false);
+      }
+      setCount(count + 1);
+      }
+    }
+      >Clicky Me for More!
+      </button>
+      <button
+        data-test="less-button"
+        onClick={() => {
+          if(count > 0) {
+            setCount(count - 1)
+          } else {
+            setError(true);
+          }
+        }}
+      >
+        Clicky Me for Less!
+      </button>
+    <div data-test="error" className={error ? "message" : "hidden"}>
+      Sorry! 0 is as low as you can go!
+    </div>
     </div>
   );
 }
--- my-solutions/click-counter/src/App.test.js
+++ my-solutions/click-counter/src/App.test.js
@@ -2,24 +2,151 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-test("renders without error",  () => {
-  const wrapper = shallow(<App/>);
-  const appComponent = wrapper.find("[data-test='component-app']");
-  expect(appComponent.length).toBe(1);
+/**
+ * Function to create a ShallowWrapper for the App component
+ * @function setup
+ * @param {object} props - component props
+ * @returns {ShallowWrapper}
+ */
+const setup = (props={}) => {return shallow(<App {...props}/>)};
+
+
+/**
+ * 
+ * @param {ShallowWrapper} wrapper 
+ * @param {string} val 
+ */
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+
+describe("<App/> Component", () => {
+  
+  it("renders without error",  () => {
+    const wrapper = setup();
+    const appComponent = findByTestAttr(wrapper, "component-app");
+    expect(appComponent.length).toBe(1);
+  });
+
+  it("renders a counter display", () => {
+    const wrapper = setup();
+    const counterDisplay = findByTestAttr(wrapper, "counter-display");
+    expect(counterDisplay.length).toBe(1);
+  });
+
+  it("starts the counter at 0", () => {
+    const wrapper = setup();
+    const count = findByTestAttr(wrapper, "count").text();
+    expect(count).toBe("0");
+  });
+
+  describe("more button", () => {
+    
+    it("renders a more button", () => {
+      const wrapper = setup();
+      const button = findByTestAttr(wrapper, "more-button");
+      expect(button.length).toBe(1);
+    });
+
+    it("clicking on more button increments counter", () => {
+      const wrapper = setup();
+      // find button
+      const button = findByTestAttr(wrapper, "more-button");
+      // click button
+      button.simulate("click");
+      // find the display and test that number matches
+      const count = findByTestAttr(wrapper, "count").text();
+      expect(count).toBe("1");
+    });
+  });
+
+  describe("less button", () => {
+    
+    it("renders a less button", () => {
+      const wrapper = setup();
+      const button = findByTestAttr(wrapper, "less-button");
+      expect(button.length).toBe(1)
+    });
+    
+    it("only decrements counter when counter is greater than 0", () => {
+      const wrapper = setup();
+
+      // make counter greater than 0
+      // find more button
+      const moreButton = findByTestAttr(wrapper, "more-button");
+      // click button
+      moreButton.simulate("click");
+      // find less button
+      const lessButton = findByTestAttr(wrapper, "less-button");
+      // click button
+      lessButton.simulate("click");
+      // find display and test number matches
+      const count = findByTestAttr(wrapper, "count").text();
+      expect(count).toBe("0");
+
+    });
+  });
+
+  describe("displays error if counter tries to go below 0", () => {
+
+    it("error does not show when not needed", () => {
+
+      const wrapper = setup();
+      const errorDiv = findByTestAttr(wrapper, "error");
+      const errorHiddenClass = errorDiv.hasClass("hidden");
+      expect(errorHiddenClass).toBe(true);
+    });
+
+    describe("counter is 0 and less is clicked", () => {
+      
+      let wrapper;
+      beforeEach(() => {
+      wrapper = setup();
+
+      // find the button and click
+      const button = findByTestAttr(wrapper, "less-button");
+      // click the button
+      button.simulate("click");
+      });
+      
+      it("shows an error", () => {
+        const errorDiv = findByTestAttr(wrapper, "error");
+        const errorHiddenClass = errorDiv.hasClass("hidden");
+        expect(errorHiddenClass).toBe(false);
+      });
+
+      test("counter still displays 0", () => {
+        const count = findByTestAttr(wrapper, "count").text();
+        expect(count).toBe("0");
+      });
+
+      it("clears the error when more is clicked", () => {
+        const button = findByTestAttr(wrapper, "more-button");
+        button.simulate("click");
+        const errorDiv = findByTestAttr(wrapper, "error");
+        const errorHiddenClass = errorDiv.hasClass("hidden");
+        expect(errorHiddenClass).toBe(true);
+      });
+  });
+    });
+    
 });
 
-test("renders a button", () => {
 
-});
 
-test("renders counter display", () => {
 
-});
 
-test("counter starts at 0", () => {
 
-});
 
-test("clicking on button increments counter", () => {
 
-});
+
+
+// test("clicking on less button decrements counter",  () => {
+//   const wrapper = setup()
+
+//   // find button
+//   const button = findByTestAttr(wrapper, "less-button");
+//   // click button
+//   button.simulate("click");
+//   // find the display and test that number mathches
+//   const count = findByTestAttr(wrapper, "count").text();
+//   expect(count).toBe("-1");
+// })
