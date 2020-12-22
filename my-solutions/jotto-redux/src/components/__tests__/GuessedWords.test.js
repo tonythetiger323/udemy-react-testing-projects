4010506ae9bfc3ab64f006ad37b0fc21ed22ef98
Tony Greeley <tonygreeley@Tonys-iMac.local>
2020-12-22 17:12:22 -0600

    Tests and code for non-zero guessed words.

--- my-solutions/jotto-redux/src/components/__tests__/GuessedWords.test.js
+++ my-solutions/jotto-redux/src/components/__tests__/GuessedWords.test.js
@@ -38,6 +38,29 @@ describe("GuessedWord component", () => {
     });
 
     describe("if there are words guessed", () => {
+        let wrapper;
+        const guessedWords = [
+            {guessedWord: "train", letterMatchCount: 3},
+            {guessedWord: "agile", letterMatchCount: 1},
+            {guessedWord: "party", letterMatchCount: 5}
+        ];
 
+        beforeEach(() => {
+            wrapper = setup({ guessedWords });
+        });
+        it("renders without error", () => {
+            const component = findByTestAttr(wrapper, "component-guessed-words");
+            expect(component.length).toBe(1);
+        });
+
+        it("renders 'guessed words' section", () => {
+            const guessedWordsNode = findByTestAttr(wrapper, "guessed-words");
+            expect(guessedWordsNode.length).toBe(1);
+        });
+
+        it("correct number of guessed words", () => {
+            const guessedWordsNodes = findByTestAttr(wrapper, "guessed-word");
+            expect(guessedWordsNodes.length).toBe(guessedWords.length);
+        });
     });
 });
