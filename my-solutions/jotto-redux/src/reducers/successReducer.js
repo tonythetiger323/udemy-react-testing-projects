d6a32fa24079d917c60ba2e8c4b481a4f3fba923
Tony Greeley <tonygreeley@Tonys-iMac.local>
2020-12-27 14:06:35 -0600

    Wrote code and tests for succesReducer.

--- my-solutions/jotto-redux/src/reducers/successReducer.js
+++ my-solutions/jotto-redux/src/reducers/successReducer.js
@@ -1,3 +1,10 @@
-export const successReducer = (state, action) => {
-    return null;
+import { actionTypes } from "../actions";
+
+export const successReducer = (state=false, action) => {
+    switch(action.type){
+        case (actionTypes.CORRECT_GUESS):
+            return true;
+        default:
+            return state;    
+    }
 };
